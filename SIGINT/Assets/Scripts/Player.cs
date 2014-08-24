4f3ec33c15d455473b35acf0fbb8373b6cba07d6
itzmattu <itzmattu@gmail.com>
2014-08-23 22:54:25 -0700

    Adding initial files because Will wants to be a big whiner.

---
+++ SIGINT/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float velocity;
+	public float speed;
+	public float maxSpeed;
+	
+	private string leftKey;
+	private string rightKey;
+	
+	private bool leftMoveEnabled;
+	private bool rightMoveEnabled;
+	
+	// Use this for initialization
+	void Start () {
+		this.leftKey = "a";
+		this.rightKey = "d";
+		
+		this.leftMoveEnabled = true;
+		this.rightMoveEnabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Update movement
+		if (Input.GetKey (this.leftKey) || Input.GetKey (this.rightKey)) {
+			this.speed += this.velocity;
+			if(this.speed > this.maxSpeed) this.speed = this.maxSpeed;
+		}
+		
+		if (Input.GetKey (this.leftKey) && this.leftMoveEnabled) {
+			gameObject.transform.Translate (Vector3.left * this.speed);
+		} else if (Input.GetKey (this.rightKey) && this.rightMoveEnabled) {
+			gameObject.transform.Translate (Vector3.right * this.speed);
+		} else {
+			this.speed = 0;
+		}
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		switch(other.name)
+		{
+		case "RightDisable":
+			Destroy(other.gameObject);
+			this.rightMoveEnabled = false;
+			break;
+		case "RightReenabled":
+			if(!this.rightMoveEnabled) {
+				Destroy(other.gameObject);
+				this.rightMoveEnabled = true;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
